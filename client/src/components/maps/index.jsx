037c869c1770b97d61a7459e8f29b446cafa15b8
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-04-07 20:29:06 -0700

    Working on google maps places functionality

--- client/src/components/maps/index.jsx
+++ client/src/components/maps/index.jsx
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
-//import {GoogleMapReact} from 'google-map-react';
+import GoogleMapReact from 'google-map-react';
+
+// Return map bounds based on list of places
+const getMapBounds = (map, maps, places) => {
+    const bounds = new maps.LatLngBounds();
+  
+    places.forEach((place) => {
+      bounds.extend(new maps.LatLng(
+        place.geometry.location.lat,
+        place.geometry.location.lng,
+      ));
+    });
+    return bounds;
+  };
+  // Re-center map when resizing the window
+const bindResizeListener = (map, maps, bounds) => {
+    maps.event.addDomListenerOnce(map, 'idle', () => {
+      maps.event.addDomListener(window, 'resize', () => {
+        map.fitBounds(bounds);
+      });
+    });
+  };
+  
+  const handleApiLoaded = (map, maps, places) => {
+      // Get bounds by our places
+      const bounds = getMapBounds(map, maps, places);
+      // Fit map to bounds
+      map.fitBounds(bounds);
+      // Bind the resize listener
+      bindResizeListener(map, maps, bounds);
+};
+
 
 class MapContainer extends Component {
 
-    render(){
-        return (
-        <div>
-            <h1>GOOGLE MAPS</h1>
-        </div>)
+    constructor(){
+        super();
+        this.state= {
+            mapsLoaded: false,
+            map: {},
+            maps: {}
+        }
     }
+
+
+  render() {
+
+    
+    return (
+    <div  className="Map_Container" style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+          bootstrapURLKeys={{
+            key: `${process.env.REACT_APP_GOOGLEKEY}`,
+            libraries: ['places', 'directions']
+          }}
+          defaultZoom={12} // Supports DP, e.g 11.5
+          defaultCenter={{ lat:47.7601, lng: -122.2054 }}
+          yesIWantToUseGoogleMapApiInternals={true}
+          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        >
+        </GoogleMapReact>
+        </div>
+          
+   )}
+  
 }
 
 export default MapContainer;
